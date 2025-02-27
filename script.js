606b9fc486d20dc2bb724daf0256b0d335130cf2
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-27 10:29:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+
+document.getElementById("btn").addEventListener("click",function(event){
+	event.preventDefault();
+
+	let age = document.getElementById("age").value.trim();
+	let name = document.getElementById("name").value.trim();
+	
+	if(!age || !name)
+	{
+		alert("Please enter valid details.");
+		return;
+	}
+	age = parseInt(age,10);
+	let check = new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>18)
+				resolve(`Welcome, . You can vote.`);
+			else
+				resolve(`Oh sorry . You aren't old enough.`)
+		},4000);
+	});
+
+	check.then(message => alert(message))
+		.catch(error => (error));
+})
